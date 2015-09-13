ca56e0b05d832d991a2e6b636564353958382b78
Losgard <patrik@losgard.se>
2015-09-13 21:53:11 +0200

    Added Postcss and Autoprefixer

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Gulp_starter
-**Gulp Starterfiles**	
-gulpfile.js  
-package.json  
+# Gulp_starter WP Genesis
+**Gulp Starterfiles**
+gulpfile.js
+package.json
 2015
--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,53 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-var sourcemaps = require('gulp-sourcemaps');
-var sass        = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var plumber     = require('gulp-plumber');
-var notify      = require("gulp-notify");
-var beeper      = require('beeper');
+var reload = browserSync.reload;
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var notify = require("gulp-notify");
+var beeper = require('beeper');
 
 // Error Helper
 function onError(err) {
-    beeper();
-    console.log(err);
+  beeper();
+  console.log(err);
 }
 
 // browser-sync task for starting the server.
 gulp.task('browser-sync', function() {
-    browserSync({
-        proxy: "mydevelopersite.dev"
-    });
+  browserSync({
+    proxy: "sitename.dev"
+  });
 });
 
 // Sass task, will run when any SCSS files change & BrowserSync
 // will auto-update browsers
-gulp.task('sass', function () {
-    gulp.src('assets/scss/**/*.scss')
-        .pipe(plumber({
-            errorHandler: onError
-        }))
+gulp.task('sass', function() {
+  gulp.src('assets/scss/**/*.scss')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(notify("Hello Gulp!!"))
+    .pipe(gulp.dest('.'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('*.css')
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(notify("Hello Gulp!!"))
-        .pipe(autoprefixer('last 2 versions'))
-        .pipe(gulp.dest('.'))
-        .pipe(reload({stream:true}));
+        .pipe(postcss([ autoprefixer({ browsers: ['last 3 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('.'));
 });
 
 // Default task to be run with `gulp`
-gulp.task('default', ['sass', 'browser-sync'], function () {
-    gulp.watch("assets/**/*.scss", ['sass']);
+gulp.task('default', ['sass', 'autoprefixer', 'browser-sync'], function() {
+  gulp.watch("assets/**/*.scss", ['sass']);
+  gulp.watch("*.css", ['autoprefixer']);
 });
